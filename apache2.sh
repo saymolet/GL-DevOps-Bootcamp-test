17943d45729cd4d1d5d54699763f5955ea3d0db2
Vladyslav Samoilenko <vladyslav.samoilenko@proton.me>
2022-11-08 16:15:28 +0200

    docs: added comments to shell script

--- apache2.sh
+++ apache2.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
 apt update
+
+# Install apache2
 apt install apache2
+
+# Config firewall rules. Allow traffic on ssh port
 ufw allow 22
+
+# Config firewall rules. Allow traffic on 80 (http) and 443 (https) port 
 ufw allow Apache
+
+# Enable ufw deamon 
 service ufw start
+
+# Restart apache2 service
 service apache2 restart
 
+# Replace default apache2 html. Now is contains name and surname
 cat <<EOF > /var/www/html/index.html
 <!DOCTYPE html>
 <html>
@@ -18,4 +29,5 @@ cat <<EOF > /var/www/html/index.html
 </html>
 EOF
 
+# Curl the localhost to ensure that script works correctly
 curl 127.0.0.1
